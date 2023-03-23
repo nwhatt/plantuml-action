c62dca0f9113b709d459b8a028fba9981ec4e72a
Nick Hatt <nick@redoxengine.com>
2023-03-23 17:13:05 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,9 @@
 #!/bin/sh -l
 
 if [ -z "$INPUT_VERSION" ]; then
-	wget -O /tmp/plantuml.jar "http://sourceforge.net/projects/plantuml/files/plantuml.jar/download"
+	wget -O /tmp/plantuml.jar "https://github.com/plantuml/plantuml/releases/download/v1.2023.4/plantuml-1.2023.4.jar"
 else
-	wget -O /tmp/plantuml.jar "http://sourceforge.net/projects/plantuml/files/plantuml.${INPUT_VERSION}.jar/download"
+	wget -O /tmp/plantuml.jar "https://github.com/plantuml/plantuml/releases/download/v${INPUT_VERSION}/plantuml-${INPUT_VERSION}.jar"
 fi
 
 java -jar /tmp/plantuml.jar "$@"
